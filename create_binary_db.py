227235ddadf00c10edcc163c32ca6378f1a643f2
Eren Golge <eren,golge@twentybn.com>
2017-06-20 18:20:06 +0200

    Make parallel bursting fucntion

--- create_binary_db.py
+++ create_binary_db.py
@@ -1,93 +1,136 @@
 import os
-import sys
 import argparse
-import glob
+import glob 
 import pickle
 import cv2
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
-from gulp_io import GulpVideoIO
+from gulpio import GulpVideoIO
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
-SMALLER_SIDE_SIZE = 320
 
-
-def shuffle(df, n=1, axis=0):
+def shuffle(df, n=1, axis=0):     
     df = df.copy()
     for _ in range(n):
         df.apply(np.random.shuffle, axis=axis)
     return df
 
 
-def resize_by_short_edge(img, size):
-    h, w = img.shape[0], img.shape[1]
+def create_chunk(inputs):
+	df = inputs[0]
+	output_folder = inputs[1]
+	chunk_no = inputs[2]
+	bin_file_path = os.path.join(output_folder, 'data{}.bin'.format(chunk_no))
+	meta_file_path = os.path.join(output_folder, 'meta{}.bin'.format(chunk_no))
+	gulp_file = GulpVideoIO(bin_file_path, 'wb', meta_file_path)
+	gulp_file.open()
+	for idx, row in df.iterrows():
+		video_id = row.youtube_id
+		label = row.label
+		start_t = row.time_start
+		end_t = row.time_end
+		folder_name = os.path.join(args.frames_path, label, video_id)+"_{:06d}_{:06d}".format(start_t, end_t)
+		imgs = glob.glob(folder_name+'/*.jpg')
+		for img in imgs:
+			img = cv2.imread(img)
+			label_idx = labels2idx[label]
+			gulp_file.write(label_idx, video_id, img)
+	gulp_file.close()
 
-    if h < w:
-        scale = w / float(h)
-        new_width = int(size * scale)
-        img = cv2.resize(img, (new_width, size))
-    else:
-        scale = h / float(w)
-        new_height = int(size * scale)
-        img = cv2.resize(img, (size, new_height))
-    return img
+
+def parallel_process(array, function, n_jobs=16, use_kwargs=False, front_num=3):
+    """
+        A parallel version of the map function with a progress bar. 
+        Args:
+            array (array-like): An array to iterate over.
+            function (function): A python function to apply to the elements of array
+            n_jobs (int, default=16): The number of cores to use
+            use_kwargs (boolean, default=False): Whether to consider the elements of array as dictionaries of 
+                keyword arguments to function 
+            front_num (int, default=3): The number of iterations to run serially before kicking off the parallel job. 
+                Useful for catching bugs
+        Returns:
+            [function(array[0]), function(array[1]), ...]
+    """
+    #We run the first few iterations serially to catch bugs
+    if front_num > 0:
+        front = [function(**a) if use_kwargs else function(a) for a in array[:front_num]]
+    #If we set n_jobs to 1, just run a list comprehension. This is useful for benchmarking and debugging.
+    if n_jobs==1:
+        return front + [function(**a) if use_kwargs else function(a) for a in tqdm(array[front_num:])]
+    #Assemble the workers
+    with ProcessPoolExecutor(max_workers=n_jobs) as pool:
+        #Pass the elements of array into function
+        if use_kwargs:
+            futures = [pool.submit(function, **a) for a in array[front_num:]]
+        else:
+            futures = [pool.submit(function, a) for a in array[front_num:]]
+        kwargs = {
+            'total': len(futures),
+            'unit': 'it',
+            'unit_scale': True,
+            'leave': True
+        }
+        #Print out the progress as tasks complete
+        for f in tqdm(as_completed(futures), **kwargs):
+            pass
+    out = []
+    #Get the results from the futures. 
+    for i, future in tqdm(enumerate(futures)):
+        try:
+            out.append(future.result())
+        except Exception as e:
+            out.append(e)
+    return front + out
 
 
-description = 'Create a binary file including all video frames with RecordIO convention.'
-p = argparse.ArgumentParser(description=description)
-p.add_argument('frames_path', type=str,
-               help=('Path to bursted frames'))
-p.add_argument('input_csv', type=str,
-               help=('Kinetics CSV file containing the following format: '
-                     'YouTube Identifier,Start time,End time,Class label'))
-p.add_argument('output_folder', type=str,
-               help='Output folder')
-args = p.parse_args()
+if __name__ == '__main__':
+	description = 'Create a binary file including all video frames with RecordIO convention.'
+	p = argparse.ArgumentParser(description=description)
+	p.add_argument('frames_path', type=str,
+	               help=('Path to bursted frames'))
+	p.add_argument('input_csv', type=str,
+	               help=('Kinetics CSV file containing the following format: '
+	                     'YouTube Identifier,Start time,End time,Class label'))
+	p.add_argument('output_folder', type=str,
+	               help='Output folder')
+	p.add_argument('vid_per_chunk', type=int, 
+				   help='number of videos in a chunk')
+	p.add_argument('num_workers', type=int,
+				   help='number of workers.')
+	args = p.parse_args()
 
-# read data csv list
-print(" > Reading data list (csv)")
-df = pd.read_csv(args.input_csv)
+	# read data csv list
+	print(" > Reading data list (csv)")
+	df = pd.read_csv(args.input_csv)
 
-# create output folder if not there
-os.makedirs(args.output_folder, exist_ok=True)
+	# create output folder if not there
+	os.makedirs(args.output_folder, exist_ok=True)
 
-# create label to idx map
-print(" > Creating label dictionary")
-labels = sorted(pd.unique(df['label']))
-assert len(labels) == 400
-labels2idx = {}
-label_counter = 0
-for label in labels:
-    labels2idx[label] = label_counter
-    label_counter += 1
-pickle.dump(labels2idx, open(args.output_folder + '/label2idx.pkl', 'wb'))
+	# create label to idx map
+	print(" > Creating label dictionary")
+	labels = sorted(pd.unique(df['label']))
+	assert len(labels) ==  400
+	labels2idx = {}
+	label_counter = 0
+	for label in labels:
+		labels2idx[label] = label_counter
+		label_counter += 1
+	pickle.dump(labels2idx, open(args.output_folder+'/label2idx.pkl', 'wb'))
 
-# shiffle df and write binary file
-print(" > Shuffling data list")
-# df = shuffle(df)
+	# shuffle df and write binary file
+	print(" > Shuffling data list")
+	df = shuffle(df)
 
-gulp_file = GulpVideoIO(args.output_folder + '/train_data.bin',
-                        'wb', args.output_folder + '/train_meta.pkl')
-gulp_file.open()
-label_count = 0
-counter = 0
-for idx, row in tqdm(df.iterrows(), total=df.shape[0]):
-    video_id = row.youtube_id
-    label = row.label
-    start_t = row.time_start
-    end_t = row.time_end
-    folder_name = os.path.join(
-        args.frames_path, label, video_id) + "_{:06d}_{:06d}".format(start_t, end_t)
-    imgs = glob.glob(folder_name + '/*.jpg')
-    # imgs = range(82)
-    for img in imgs:
-        img = cv2.imread(img)
-        img = resize_by_short_edge(img, SMALLER_SIDE_SIZE)
-        # img = np.random.randint(0, 255, size=[320,320,3])
-        label_idx = labels2idx[label]
-        gulp_file.write(label_idx, video_id, img)
+	# set input array
+	inputs = []
+	for idx, df_sub in df.groupby(np.arange(len(df))//args.vid_per_chunk):
+		input_data = [df_sub, args.output_folder, idx]
+		inputs.append(input_data)
+		parallel_process(inputs, create_chunk, n_jobs=args.num_workers)
 
-    counter += 1
-    if counter == 5:
-        break
-gulp_file.close()
+
+
+
+
