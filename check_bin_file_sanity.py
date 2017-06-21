41fe5ca4d558586a5d69c5e6f7599ae00975b4f9
Raghav Goyal <raghavgoyal14@gmail.com>
2017-06-21 06:04:54 -0700

    minor change

--- check_bin_file_sanity.py
+++ check_bin_file_sanity.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
 
     gulp_file = GulpVideoIO(path_bin, 'rb', path_meta)
     gulp_file.open()
-    pprint(gulp_file.meta_dict['3yaoNwz99xM'])
-    sys.exit()
+    # pprint(gulp_file.meta_dict['3yaoNwz99xM'])
+    # sys.exit()
     for i in range(len(gulp_file.meta_dict[vid_id])):
         img = gulp_file.read(gulp_file.meta_dict[vid_id][i])
         img.save(os.path.join(vid_path, "frame%04d.jpg" % (i + 1)))
