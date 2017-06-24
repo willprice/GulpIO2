f049fa5453633d3532f38a55d38488180d35733a
Raghav Goyal <raghavgoyal14@gmail.com>
2017-06-24 02:29:07 -0700

    sanity mods

--- check_bin_file_sanity.py
+++ check_bin_file_sanity.py
@@ -11,16 +11,16 @@ def save_video():
 
 if __name__ == "__main__":
     path_to_chunk = "/media/4TBSATA/kinetics/binary_data/from_videos/validation_ffmpeg/"
-    path_bin = os.path.join(path_to_chunk, "data0.bin")
-    path_meta = os.path.join(path_to_chunk, "meta0.bin")
+    path_bin = os.path.join(path_to_chunk, "data32.bin")
+    path_meta = os.path.join(path_to_chunk, "meta32.bin")
 
-    vid_id = "6IbvOJxXnOo"
+    vid_id = "zQa0VbID0i4"
     vid_path = os.path.join("sample_vids", vid_id)
     os.makedirs(vid_path, exist_ok=True)
 
     gulp_file = GulpVideoIO(path_bin, 'rb', path_meta)
     gulp_file.open()
-    pprint(gulp_file.meta_dict['6IbvOJxXnOo'])
+    pprint(gulp_file.meta_dict['zQa0VbID0i4'])
     for i in range(len(gulp_file.meta_dict[vid_id])):
         img = gulp_file.read(gulp_file.meta_dict[vid_id][i])
         img.save(os.path.join(vid_path, "frame%04d.jpg" % (i + 1)))
