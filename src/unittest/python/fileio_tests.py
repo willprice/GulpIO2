a98efe4f0e961d55d2b9da0e7be3392106a763b9
Susanne Westphal <westphal.susanne@gmx.de>
2017-07-31 15:48:33 +0200

    added test for gulpioVideoIO init function

--- src/unittest/python/fileio_tests.py
+++ src/unittest/python/fileio_tests.py
@@ -8,6 +8,7 @@ import unittest.mock as mock
 
 from gulpio.fileio import (calculate_chunks,
                            GulpVideoIO,
+                           json_serializer
                            )
 
 
@@ -36,5 +37,22 @@ class TestCalculateChunks(unittest.TestCase):
 
 
 class TestGulpVideoIO(unittest.TestCase):
+    @mock.patch('gulpio.fileio.json_serializer')
+    def test_initializer(self, mock_json_serializer):
+        path = "ANY_PATH"
+        flag = "ANY_FLAG"
+        meta_path = "ANY_META_PATH"
+        img_info_path = "ANY_IMG_INFO_PATH"
+        gulpio_video_io = GulpVideoIO(path, flag, meta_path, img_info_path,
+                                      mock_json_serializer)
+        self.assertEqual(path, gulpio_video_io.path)
+        self.assertEqual(flag, gulpio_video_io.flag)
+        self.assertEqual(meta_path, gulpio_video_io.meta_path)
+        self.assertEqual(img_info_path, gulpio_video_io.img_info_path)
+        self.assertEqual(mock_json_serializer, gulpio_video_io.serializer)
 
-
+        self.assertEqual(gulpio_video_io.is_open, False)
+        self.assertEqual(gulpio_video_io.is_writable, False)
+        self.assertEqual(gulpio_video_io.f, None)
+        self.assertEqual(gulpio_video_io.img_dict, None)
+        self.assertEqual(gulpio_video_io.meta_dict, None)
