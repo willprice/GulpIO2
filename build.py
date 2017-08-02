ad7819a3df4896e553521a268897e339a92a9c6b
Valentin Haenel <valentin@haenel.co>
2017-08-02 16:52:39 +0200

    upgrade the build.py

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
-from pybuilder.core import use_plugin, init
+from pybuilder.core import use_plugin, init, Author
+from pybuilder.vcs import count_travis
 
 use_plugin("python.core")
 use_plugin("python.unittest")
@@ -6,10 +7,20 @@ use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
+use_plugin("filter_resources")
 
 
 name = "gulpio"
 default_task = "publish"
+version = count_travis()
+summary = "Binary storage format for deep learning on videos."
+authors = [Author("Eren Golge", "eren.golge@twentybn.com"),
+           Author("Raghav Goyal", "raghav.goyal@twentybn.com"),
+           Author("Susanne Westphal", "susanne.westphal@twentybn.com"),
+           Author("Valentin Haenel", "valentin.haenel@twentybn.com"),
+           ]
+
+requires_python = ">=3.4"
 
 
 @init
@@ -19,3 +30,7 @@ def set_properties(project):
     project.depends_on('docopt')
     project.depends_on('Pillow')
     project.depends_on('sh')
+    project.get_property('filter_resources_glob').extend(
+        ['**/gulpio/__init__.py'])
+    project.get_property('coverage_exceptions').extend(
+        ['gulpio.adapters'])
