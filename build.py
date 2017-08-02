c24e7f7aa4018c1f08438712bd92f57da9058b96
Valentin Haenel <valentin@haenel.co>
2017-08-02 16:43:05 +0200

    too low coverage now breaks the build, once again

--- build.py
+++ build.py
@@ -14,7 +14,6 @@ default_task = "publish"
 
 @init
 def set_properties(project):
-    project.set_property('coverage_break_build', False)
     project.depends_on('tqdm')
     project.depends_on('opencv-python')
     project.depends_on('docopt')
