07a2c4288b2ce5caaa82d17fcde95eedefdf180f
Will Price <will.price94@gmail.com>
2021-02-27 10:15:32 +0000

    Release v0.0.3

--- src/gulpio2/__version__.py
+++ src/gulpio2/__version__.py
@@ -1,5 +1,5 @@
 __title__ = "gulpio2"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __copyright__ = 'Copyright 2021 Will Price & TwentyBN'
 __description__ = "Binary storage format for deep learning on videos."
 __author__ = """\
